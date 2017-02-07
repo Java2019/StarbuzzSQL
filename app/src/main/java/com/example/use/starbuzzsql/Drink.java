eb2c3d61fbac6ebec0dbfd4260c2cddded2244bd
java2019 <java_2019@mail.ru>
2017-02-07 12:02:59 +0300

    Initial commit

---
+++ app/src/main/java/com/example/use/starbuzzsql/Drink.java
@@ -0,0 +1,42 @@
+package com.example.use.starbuzzsql;
+
+/**
+ * Created by use on 04.02.17.
+ */
+public class Drink {
+    private String name;
+    private String description;
+    private int imageResourceId;
+
+    public static final Drink[] drinks = {
+            new Drink("Latte", "A couple of espresso shots with steamed milk",
+                            R.drawable.latte),
+            new Drink("Cappuccino", "Espresso, hot milk, and a steamed milk foam",
+                    R.drawable.cappuccino),
+            new Drink("Filter", "Highest quality beans roasted and brewed fresh",
+                    R.drawable.filter)
+    };
+
+    public Drink(String name, String description, int imageResourceId) {
+        this.name = name;
+        this.description = description;
+        this.imageResourceId = imageResourceId;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getImageResourceId() {
+        return imageResourceId;
+    }
+}
